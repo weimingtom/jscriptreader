4d78a2f6e6bd66ba068a91f6c11e84d7fb0ecf3c
xuyang22 <xuyang22@0aa1e6aa-cb59-11dd-a880-a1081c7ac358>
2008-12-16 10:14:02 +0000

    first commit, upload what I have done at local computer

---
+++ Program.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)Program.java
+ *
+ *
+ * @author Arthur
+ * @version 1.00 2008/12/15
+ */
+
+public class Program 
+{    
+    //
+    // @param args the command line arguments
+    //
+    public static void main(String[] args) 
+    {
+        FormMain formMain = new FormMain();
+        formMain.setVisible(true);
+    }
+}
